0a58d1490f7e9740a51510554b34c2884c349226
pieterjooste <pieter.jooste.jp@gmail.com>
2024-03-28 16:05:55 +0200

    Development 28/03/2024 Version 2.0

--- Child Health/app/build.gradle.kts
+++ Child Health/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 3
-        versionName = "1.3"
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
